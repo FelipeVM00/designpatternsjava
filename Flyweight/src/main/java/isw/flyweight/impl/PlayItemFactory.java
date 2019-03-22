0c277a4a39c0729142f581f34e3f17cc55a7852d
FelipeVM00 <36248693+FelipeVM00@users.noreply.github.com>
2019-03-21 19:15:26 -0500

    Add files via upload

---
+++ Flyweight/src/main/java/isw/flyweight/impl/PlayItemFactory.java
@@ -0,0 +1,19 @@
+package isw.flyweight.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlayItemFactory {
+	public static boolean enableFlyweight = true;
+	private static final Map<String,PlayItem> PLAY_ITEMS = new HashMap<>();
+	private static Long idSequence = 0L;
+	public static PlayItem createPlayItem(String songName){
+		if(enableFlyweight && PLAY_ITEMS.containsKey(songName)){
+			return PLAY_ITEMS.get(songName);
+		}else{
+			PlayItem playItem = new PlayItem(++idSequence, songName);
+			PLAY_ITEMS.put(songName, playItem);
+			return playItem;
+		}
+	}
+}
